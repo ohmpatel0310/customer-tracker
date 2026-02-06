32ab403936326d1f675a4f884910d4b475641a74
Ohm Patel <ohmp3@usf.edu>
2026-02-06 12:41:15 -0500

    Added and Removed Customer Data

--- cc_3.js
+++ cc_3.js
@@ -1,3 +1,6 @@
+
+// Step 2: Creating Customer Records
+
 let customers = [
     {
         name: "Jimmy John",
@@ -16,3 +19,15 @@ let customers = [
         purchases: ["Panda Brownie"],
     }
 ]
+
+// Step 3: Adding and Removing Data
+
+customers.push({
+    name: "Five Guys",
+    email: "fiveguys@gmail.com",
+    purchases: "Brownie for Guys",
+})
+
+customers.shift();
+
+console.log(customers);
