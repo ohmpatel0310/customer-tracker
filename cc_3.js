3c930d553a8e1b53f12f278fe9f3328c44ba3695
Ohm Patel <ohmp3@usf.edu>
2026-02-06 12:34:33 -0500

    Initial Commit to Create Customers Records

---
+++ cc_3.js
@@ -0,0 +1,18 @@
+let customers = [
+    {
+        name: "Jimmy John",
+        email: "jimmyjohn@gmail.com",
+        purchases: ["Chocolate Brownie", "Sandwich Brownie"],
+
+    },
+    {
+        name: "Dave Chicken",
+        email: "davechicken@gmail.com",
+        purchases: ["Chicken Brownie"],
+    },
+    {
+        name: "Panda Express",
+        email: "pandaexpress@gmail.com",
+        purchases: ["Panda Brownie"],
+    }
+]
